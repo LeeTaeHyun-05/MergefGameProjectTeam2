4158e84814d140936284e62e2b8ddd841e05e6c5
soyoonL <diamomo777@naver.com>
2025-06-06 14:04:08 +0900

    사운드 조절

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,9 +7,6 @@ public class ButtonManager : MonoBehaviour
 {
     public GameObject Setting;
     public GameObject SkillPopup;
-    public GameObject Stage1Target;
-    public GameObject Stage2Target;
-    public GameObject Stage3Target;
     public void GameStart()
     {
         SceneManager.LoadScene("MainMenu");
@@ -45,50 +41,5 @@ public class ButtonManager : MonoBehaviour
     public void ToStart()
     {
         SceneManager.LoadScene("StartScene");
-    }
-
-    public void Stage1_TargetOpen()
-    {
-        Stage1Target.SetActive(true); 
-    }
-
-    public void Stage1_TargetQuit()
-    {
-        Stage1Target.SetActive(false );
-    }    
-
-    public void Stage1_Start()
-    {
-        SceneManager.LoadScene("Stage1");
-    }
-
-    public void Stage2_TargetOpen()
-    {
-        Stage2Target.SetActive(true);
-    }
-
-    public void Stage2_TargetQuit()
-    {
-        Stage2Target.SetActive(false);
-    }
-
-    public void Stage2_Start()
-    {
-        SceneManager.LoadScene("Stage2");
-    }
-
-    public void Stage3_TargetOpen()
-    {
-        Stage3Target.SetActive(true);   
-    }
-
-    public void Stage3_TargetQuit()
-    {
-        Stage3Target.SetActive(false);  
-    }
-
-    public void Stage3_Start()
-    {
-        SceneManager.LoadScene("Stage3");
     }
 }
